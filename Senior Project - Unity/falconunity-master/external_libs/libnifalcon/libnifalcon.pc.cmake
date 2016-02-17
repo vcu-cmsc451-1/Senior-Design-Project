b546f94f0e84c652c14d9c1c0df9935b9f2be301
Davis24 <davisma24@vcu.edu>
2016-02-17 14:48:44 -0500

    Revert "Falcon branch"

--- Senior Project - Unity/falconunity-master/external_libs/libnifalcon/libnifalcon.pc.cmake
+++
@@ -1,9 +0,0 @@
-libdir=@CMAKE_INSTALL_PREFIX@/lib
-includedir=@CMAKE_INSTALL_PREFIX@/include
-
-Name: @PC_SHORT_NAME@
-Description: @PC_LONG_NAME@: @PC_DESCRIPTION@
-Requires: @PC_REQUIRES@
-Version: @PC_VERSION@
-Libs: -L${libdir} @PC_LINK_FLAGS@
-Cflags: -I${includedir} @PC_COMPILE_FLAGS@
--- libnifalcon-master/examples/falcon_test_cli/FalconLoopTimeTest.cpp
+++
@@ -1,7 +0,0 @@
-#include "FalconLoopTimeTest.h"
-#include "falcon/core/FalconDevice.h"
-
-void FalconLoopTimeTest::runFunction()
-{
-	m_falconDevice->runIOLoop();
-}
